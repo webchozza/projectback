5aae1f3dbfd7e8a245bec0fa633c6e8b57c1a6f8
Koo <s2_music@naver.com>
2017-04-24 23:55:27 +0900

    BoardVO 게시판 구분 문자열 추가 (쓸 사람만 쓰면 되요. 안써도 무방)

--- src/main/java/first/com/model/BoardVO.java
+++ src/main/java/first/com/model/BoardVO.java
@@ -2,11 +2,10 @@ package first.com.model;
 
 public class BoardVO {
 	
-	private int qna = 0;//Qna 게시판
-	private int free = 0;//자유게시판(커뮤니티)
-	private int code = 0;//오픈소스 게시판
-	private int job = 0;//구인구직 게시판
-	
+	private int code = 1;//오픈소스 게시판
+	private int free = 2;//자유게시판(커뮤니티)
+	private int job = 3;//구인구직 게시판
+	private int qna = 4;//Qna 게시판
 	
 	public int getQna() {
 		return qna;
