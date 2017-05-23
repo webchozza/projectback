56e0ef654a214b1c98e8c3cb518b6f1b372020aa
Koo <s2_music@naver.com>
2017-05-23 18:58:15 +0900

    Revert "Revert "학준형만 남아따  오류잡져""
    This reverts commit 9509132d2c5f94ddbf991f00c51f540b03fe2e29.

--- src/main/java/first/com/controller/bqna/BqnaDetail.java
+++ src/main/java/first/com/controller/bqna/BqnaDetail.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import first.com.dao.BqnaDAO;
+import first.com.dao.RecommendDAO;
 import first.com.dao.ScrapDAO;
 import first.com.model.BcommentDTO;
 import first.com.model.BoardDTO;
@@ -24,6 +25,9 @@ public class BqnaDetail {
 	
 	@Resource
 	private ScrapDAO Scrap;
+	
+	@Resource 
+	private RecommendDAO recommendSerivce;
 
 	@RequestMapping(value = "/bqnadetail")
 	public ModelAndView bqnaDetail(@RequestParam(value = "currentPage", defaultValue="1") int currentPage,
@@ -36,23 +40,19 @@ public class BqnaDetail {
 		List<BcommentDTO> bcqnaList = bqnaService.bcqnaList(board_id);
 		bqnaService.bqnaUpdateHit(board_id);
 		
-		//답변체크 값 가져오는 로직
 		int answerCheck = bqnaService.bqnaAnswerCheck(board_id);
 		if(answerCheck > 0){
-			mav.addObject("answerCheck", 1);//답변이 채택된 댓글이 있는 게시물이면
+			mav.addObject("answerCheck", 1);
 		}else{
-			mav.addObject("answerCheck", -1);//답변이 채택된 댓글이 업는 게시물이면
+			mav.addObject("answerCheck", -1);
 		}
-		//
 		
-		//답변채택글 뽑아서 모델객체에 저장
 		for(int i=0;i<bcqnaList.size(); i++){
 			if(bcqnaList.get(i).getBcomment_id() == bcqnaList.get(i).getAnswer()){
 				mav.addObject("answercomment", bcqnaList.get(i));
 				break;
 			}
 		}
-		//
 		
 
 		mav.addObject("board_tag", bqnaDetail.getBoard_tag());
@@ -61,13 +61,16 @@ public class BqnaDetail {
 		mav.addObject("bcqnaList", bcqnaList);
 		mav.setViewName("QnaDetail");
 
+		//add by eongoo
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("session_id", session_id);
 		map.put("board_id", board_id);
-		if (session_id == -1) {
+		if(session_id == -1){ 
 			mav.addObject("scrapCheck", "-1");
+			mav.addObject("recommendCheck", "-1");
 		} else {
 			mav.addObject("scrapCheck", Scrap.scrapCheck(map));
+			mav.addObject("recommendCheck", recommendSerivce.recommendCheck(map));
 		}
 
 		return mav;
